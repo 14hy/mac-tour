dc13e555c80041c767033ecabef6899168f88fa1
Seung-Min Baek <40845064+Juyudang@users.noreply.github.com>
2019-09-19 03:39:53 +0900

    🔀 아이콘, 앱 이름, 스플래쉬 스크린 구현 (#7)
    * install plugin splashscreen
    
    * #6 - add icon, set splashscreen, rename appname

--- client/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ client/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,7 +1,33 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "es6-promise-plugin.Promise",
+      "file": "plugins/es6-promise-plugin/www/promise.js",
+      "pluginId": "es6-promise-plugin",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-screen-orientation.screenorientation",
+      "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
+      "pluginId": "cordova-plugin-screen-orientation",
+      "clobbers": [
+        "cordova.plugins.screenorientation"
+      ]
+    }
+  ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-browsersync": "0.1.7"
+    "cordova-plugin-browsersync": "0.1.7",
+    "cordova-plugin-splashscreen": "5.0.4-dev",
+    "es6-promise-plugin": "4.2.2",
+    "cordova-plugin-screen-orientation": "3.0.2"
   };
 });
